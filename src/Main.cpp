477e8c6019d22a66f9eea5120e4ed14e2e301f55
Alban <godier.alban@gmail.com>
2023-12-06 14:04:28 +0100

    update

--- src/Main.cpp
+++ src/Main.cpp
@@ -10,13 +10,41 @@ App::App() {
 	this->InitializeComponent();
 }
 
+// ========================================================
+// =				      GENERAL                         =
+// ========================================================
+
 void App::InitializeComponent() {
+	// TODO ajoutez la fenêtre de connexion
+	// 
+
+	// On rappelle le constructeur de la classe mère
 	__super::InitializeComponent();
 
+	// Modification du titre de la fenêtre pour correspondre à l'onget sélectionné
+	this->Text = "Client - " + ConfigurationManager::AppSettings["app_name"];
+	// Modification du titre de l'application
+	this->label_Logo->Text = ConfigurationManager::AppSettings["app_name"];
+
+	// Modification des textBox pour ajouter un placeholder et une sortie de focus lors de l'appui sur la touche entrée
 	gcnew ElementsCustomization::SearchInputBox(this->textBox_ClientsSearch, "Search");
 	gcnew ElementsCustomization::SearchInputBox(this->textBox_StockSearch, "Search");
 	gcnew ElementsCustomization::SearchInputBox(this->textBox_OrdersSearch, "Search");
+	gcnew ElementsCustomization::SearchInputBox(this->textBox_OrdersClientsSearch, "Search");
+	gcnew ElementsCustomization::SearchInputBox(this->textBox_OrdersStockSearch, "Search");
+
 }
+
+// Permet de changer le nom de la fenêtre en fonction de l'onglet sélectionné
+void App::tabControl_Tabs_SelectedIndexChanged(System::Object^ sender, System::EventArgs^ e) {
+	this->Text = this->tabControl_Tabs->SelectedTab->Text + " - " + "FirmManager";
+}
+
+// ========================================================
+// =                      HOME TAB                        =
+// ========================================================
+
+// Ouverture des liens de la page d'accueil
 void App::linkLabel_Help_LinkClicked(System::Object^ sender, System::Windows::Forms::LinkLabelLinkClickedEventArgs^ e) {
 	openLink("https://github.com/CESI-ORLEANS-A2/Groupe-3-Projet-Bloc-POO", this->linkLabel_Help);
 }
--- src/Utils.h
+++ src/Utils.h
@@ -4,32 +4,42 @@ using namespace System;
 using namespace System::Drawing;
 using namespace System::Windows;
 using namespace System::Windows::Forms;
+using namespace System::Text::RegularExpressions;
 
 namespace Groupe3ProjetBlocPOO {
-	void openLink(String^ link);
-	void openLink(String^ link, LinkLabel^ label);
+	namespace Utils {
+		void openLink(String^ link);
+		void openLink(String^ link, LinkLabel^ label);
+		Int64^ toTimestamp();
+		Int64^ toTimestamp(DateTime^);
+		DateTime^ fromTimestamp(Int64^ timestamp);
 
-	namespace ElementsCustomization {
-		public ref class CustomizeTextBox {
-		public:
-			CustomizeTextBox();
-			CustomizeTextBox(TextBox^ textBox, String^ placeholder);
+		namespace ElementsCustomization {
+			public ref class CustomizeTextBox {
+			public:
+				CustomizeTextBox();
+				CustomizeTextBox(TextBox^ textBox, String^ placeholder);
+				CustomizeTextBox(TextBox^ textBox, String^ placeholder, String^ regexValidation, RegexOptions regexOptions);
 
-		protected:
-			void __onFocus(Object^ sender, EventArgs^ e);
-			void __onUnfocus(Object^ sender, EventArgs^ e);
+			protected:
+				void __onFocus(Object^ sender, EventArgs^ e);
+				void __onUnfocus(Object^ sender, EventArgs^ e);
+				void __onTextChanged(Object^ sender, EventArgs^ e);
 
-			TextBox^ __textBox;
-			String^ __placeholder;
-		};
+				TextBox^ __textBox;
+				String^ __placeholder;
+				String^ __regexValidation;
+				RegexOptions __regexOptions;
+			};
 
-		public ref class SearchInputBox : public CustomizeTextBox {
-		public :
-			SearchInputBox();
-			SearchInputBox(TextBox^ textBox, String^ placeholder);
+			public ref class SearchInputBox : public CustomizeTextBox {
+			public:
+				SearchInputBox();
+				SearchInputBox(TextBox^ textBox, String^ placeholder);
 
-		protected:
-			void __onKeyDown(Object^ sender, KeyEventArgs^ e);
-		};
+			protected:
+				void __onKeyDown(Object^ sender, KeyEventArgs^ e);
+			};
+		}
 	}
 }
